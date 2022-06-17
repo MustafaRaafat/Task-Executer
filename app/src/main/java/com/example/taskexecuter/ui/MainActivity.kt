db5bc16871e38e4faa22d625f2c718ee226339aa
mustafa <mustafa.raft@hotmail.com>
2022-06-17 17:44:55 +0200

    added coroutines and run successfully

--- app/src/main/java/com/example/taskexecuter/ui/MainActivity.kt
+++ app/src/main/java/com/example/taskexecuter/ui/MainActivity.kt
@@ -3,20 +3,24 @@ package com.example.taskexecuter.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.taskexecuter.R
 import com.example.taskexecuter.databinding.ActivityMainBinding
 import com.example.taskexecuter.ui.adapters.TaskAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.*
 import kotlin.collections.ArrayList
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     val taskAdapter = TaskAdapter()
     val data = ArrayList<String>()
-    val ss= SimpleDateFormat("dd-MM-yyyy hh:mm")
-    var curr=ss.format(Date())
+    val ss = SimpleDateFormat("dd-MM-yyyy hh:mm")
+    var curr = ss.format(Date())
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +28,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val view = binding.root
         setContentView(view)
         binding.taskRecyclerView.adapter = taskAdapter
-
         binding.task1But.setOnClickListener(this)
         binding.task2But.setOnClickListener(this)
         binding.task3But.setOnClickListener(this)
@@ -34,25 +37,65 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(p0: View?) {
-        if (p0 ==binding.task1But){
-            curr=ss.format(Date())
-            data.add(curr+" <b>Task 1</b>")
-            taskAdapter.setdata(data)
+        when (p0) {
+            binding.task1But -> task1()
+            binding.task2But -> task2()
+            binding.task3But -> task3()
+            binding.task4But -> task4()
         }
-        if (p0 ==binding.task2But){
-            curr=ss.format(Date())
-            data.add(curr+" <b>Task 2</b>")
-            taskAdapter.setdata(data)
+
+    }
+
+    fun task1() {
+        runBlocking {
+            flow<String> {
+                emit("Task 1")
+
+            }.collect {
+                curr = ss.format(Date())
+                data.add(curr + " <b>" + it + "</b>")
+                taskAdapter.setdata(data)
+            }
         }
-        if (p0 ==binding.task3But){
-            curr=ss.format(Date())
-            data.add(curr+" <b>Task 3</b>")
-            taskAdapter.setdata(data)
+
+    }
+
+    fun task2() {
+        runBlocking {
+            flow<String> {
+                emit("Task 2")
+
+            }.collect {
+                curr = ss.format(Date())
+                data.add(curr + " <b>" + it + "</b>")
+                taskAdapter.setdata(data)
+            }
         }
-        if (p0 ==binding.task4But){
-            curr=ss.format(Date())
-            data.add(curr+" <b>Task 4</b>")
-            taskAdapter.setdata(data)
+    }
+
+    fun task3() {
+        runBlocking {
+            flow<String> {
+                emit("Task 3")
+
+            }.collect {
+                curr = ss.format(Date())
+                data.add(curr + " <b>" + it + "</b>")
+                taskAdapter.setdata(data)
+            }
+        }
+    }
+
+    fun task4() {
+        runBlocking {
+            flow<String> {
+                emit("Task 4")
+
+            }.collect {
+                curr = ss.format(Date())
+                data.add(curr + " <b>" + it + "</b>")
+                taskAdapter.setdata(data)
+            }
         }
     }
 }
